8abd3d2adfb0ae38797cb402aea944a8ca3beff6
Moejay10 <mohe95@hotmail.no>
2020-03-19 09:48:12 +0100

    small change

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -408,7 +408,7 @@ if (Task == "b")
     int numberOfParticles;
     int numberOfDimensions;
     double omega            = 1.0;          // Oscillator frequency.
-    double alpha            = 0.2;          // Variational parameter.
+    double alpha;          // Variational parameter.
     double beta             = 2.82843;      // Variational parameter.
     double gamma            = beta;         // Variational parameter.
     double a                = 0.0043;       // Interaction parameter.
@@ -436,6 +436,11 @@ if (Task == "b")
     cout << "\n" << "The number of Dimensions: " << endl;
     cout << "\n" << "Write here " << endl;
     cin >> numberOfDimensions;
+
+
+    cout << "\n" << "The variational parameter alpha: " << endl;
+    cout << "\n" << "Write here " << endl;
+    cin >> alpha;
 
 
     // Analyitcal Run
