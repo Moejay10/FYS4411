8e5506fb913e133c41f53e39ebb1deb58a860f30
Moejay10 <mohe95@hotmail.no>
2020-05-14 14:43:25 +0200

    small change

--- Project2/Code/main.cpp
+++ Project2/Code/main.cpp
@@ -141,8 +141,8 @@ int main() {
     cout << "-------------- \n" << "Brute Force \n" << "-------------- \n" << endl;
 
     // Choose which file to write to
-    //string file = "Python/Results/Statistical_Analysis/BF_Blocking_eta_10^" + to_string(gamma) + "_inputs_" + to_string(numberOfInputs) + ".dat";
-    string file = "Python/Results/Statistical_Analysis/Interaction_BF_Blocking_eta_10^" + to_string(gamma) + "_inputs_" + to_string(numberOfInputs) + ".dat";
+    //string file = "Python/Results/Statistical_Analysis/BF_Blocking_eta_10^" + to_string(gamma) + "_hidden_" + to_string(numberOfHidden) + "_inputs_" + to_string(numberOfInputs) + ".dat";
+    string file = "Python/Results/Statistical_Analysis/Interaction_BF_Blocking_eta_10^" + to_string(gamma) + "_hidden_" + to_string(numberOfHidden) + "_inputs_" + to_string(numberOfInputs) + ".dat";
 
     ofile.open(file);
     ofile << setiosflags(ios::showpoint | ios::uppercase);
@@ -163,8 +163,8 @@ int main() {
 
 
     // Write to file
-    //file = "Python/Results/Brute_Force/Energies_eta_10^" + to_string(gamma) + "_inputs_" + to_string(numberOfInputs) + ".dat";
-    file = "Python/Results/Brute_Force/Interaction_Energies_eta_10^" + to_string(gamma) + "_inputs_" + to_string(numberOfInputs) + ".dat";
+    //file = "Python/Results/Brute_Force/Energies_eta_10^" + to_string(gamma) + "_hidden_" + to_string(numberOfHidden) + "_inputs_" + to_string(numberOfInputs) + ".dat";
+    file = "Python/Results/Brute_Force/Interaction_Energies_eta_10^" + to_string(gamma) + "_hidden_" + to_string(numberOfHidden) + "_inputs_" + to_string(numberOfInputs) + ".dat";
 
     ofile.open(file);
     ofile << setiosflags(ios::showpoint | ios::uppercase);
@@ -186,8 +186,8 @@ int main() {
     cout << "-------------- \n" << "Importance Sampling \n" << "-------------- \n" << endl;
 
     // Choose which file to write to
-    //string file = "Python/Results/Statistical_Analysis/IS_Blocking_eta_10^" + to_string(gamma) + "_inputs_" + to_string(numberOfInputs) + ".dat";
-    string file = "Python/Results/Statistical_Analysis/Interaction_IS_Blocking_eta_10^" + to_string(gamma) + "_inputs_" + to_string(numberOfInputs) + ".dat";
+    //string file = "Python/Results/Statistical_Analysis/IS_Blocking_eta_10^" + to_string(gamma) + "_hidden_" + to_string(numberOfHidden) + "_inputs_" + to_string(numberOfInputs) + ".dat";
+    string file = "Python/Results/Statistical_Analysis/Interaction_IS_Blocking_eta_10^" + to_string(gamma) + "_hidden_" + to_string(numberOfHidden) + "_inputs_" + to_string(numberOfInputs) + ".dat";
 
     ofile.open(file);
     ofile << setiosflags(ios::showpoint | ios::uppercase);
@@ -210,8 +210,8 @@ int main() {
 
 
     // Write to file
-    //file = "Python/Results/Importance_Sampling/Energies_eta_10^" + to_string(gamma) + "_inputs_" + to_string(numberOfInputs) + ".dat";
-    file = "Python/Results/Importance_Sampling/Interaction_Energies_eta_10^" + to_string(gamma) + "_inputs_" + to_string(numberOfInputs) + ".dat";
+    //file = "Python/Results/Importance_Sampling/Energies_eta_10^" + to_string(gamma) + "_hidden_" + to_string(numberOfHidden) + "_inputs_" + to_string(numberOfInputs) + ".dat";
+    file = "Python/Results/Importance_Sampling/Interaction_Energies_eta_10^" + to_string(gamma) + "_hidden_" + to_string(numberOfHidden) + "_inputs_" + to_string(numberOfInputs) + ".dat";
 
     ofile.open(file);
     ofile << setiosflags(ios::showpoint | ios::uppercase);
@@ -237,8 +237,8 @@ int main() {
     gibbs = 2;
 
     // Choose which file to write to
-    //string file = "Python/Results/Statistical_Analysis/Gibbs_Blocking_eta_10^" + to_string(gamma) + "_inputs_" + to_string(numberOfInputs) + ".dat";
-    string file = "Python/Results/Statistical_Analysis/Interaction_Gibbs_Blocking_eta_10^" + to_string(gamma) + "_inputs_" + to_string(numberOfInputs) + ".dat";
+    //string file = "Python/Results/Statistical_Analysis/Gibbs_Blocking_eta_10^" + to_string(gamma) + "_hidden_" + to_string(numberOfHidden) + "_inputs_" + to_string(numberOfInputs) + ".dat";
+    string file = "Python/Results/Statistical_Analysis/Interaction_Gibbs_Blocking_eta_10^" + to_string(gamma) + "_hidden_" + to_string(numberOfHidden) + "_inputs_" + to_string(numberOfInputs) + ".dat";
 
     ofile.open(file);
     ofile << setiosflags(ios::showpoint | ios::uppercase);
@@ -258,8 +258,8 @@ int main() {
     ofile.close();
 
     // Write to file
-    //file = "Python/Results/Gibbs/Energies_eta_10^" + to_string(gamma) + "_inputs_" + to_string(numberOfInputs) + ".dat";
-    file = "Python/Results/Gibbs/Interaction_Energies_eta_10^" + to_string(gamma) + "_inputs_" + to_string(numberOfInputs) + ".dat";
+    //file = "Python/Results/Gibbs/Energies_eta_10^" + to_string(gamma) + "_hidden_" + to_string(numberOfHidden) + "_inputs_" + to_string(numberOfInputs) + ".dat";
+    file = "Python/Results/Gibbs/Interaction_Energies_eta_10^" + to_string(gamma) + "_hidden_" + to_string(numberOfHidden) + "_inputs_" + to_string(numberOfInputs) + ".dat";
 
     ofile.open(file);
     ofile << setiosflags(ios::showpoint | ios::uppercase);
